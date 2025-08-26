854140881574c08804797d0959ad9bed4b4424c0
Paulo Henrique de Jesus <84482254+PauloHenriqueJr@users.noreply.github.com>
2025-08-26 02:59:05 -0300

    feat: add analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { initAnalytics } from './integrations/analytics/googleAnalytics';
 
-createRoot(document.getElementById("root")!).render(<App />);
+initAnalytics();
+createRoot(document.getElementById('root')!).render(<App />);
